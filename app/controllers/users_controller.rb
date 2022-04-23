ef7caea9cc18b631cb098a2f4a664edd6b52cfbc
sunilprakash97 <sunilprakash97@gmail.com>
2022-04-22 23:41:31 -0400

    make it better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user! 
+
+  def index
+    @users = User.search(params[:term])
+    respond_to :js
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:photos, :likes, :comments)

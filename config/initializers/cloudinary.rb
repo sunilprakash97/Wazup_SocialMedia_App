c23c11958ed88c4d4338385396e077c68b7ea536
sunilprakash97 <sunilprakash97@gmail.com>
2022-04-22 22:25:18 -0400

    make it better

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,6 @@
 Cloudinary.config do |config|
-    config.cloud_name = ENV["cloudinary_cloud_name"]
-    config.api_key = ENV["cloudinary_api_key"]
-    config.api_secret = ENV["cloudinary_api_secret"]
+    config.cloud_name = "dxopyewoa"
+    config.api_key = "359386819188894"
+    config.api_secret = "PiPFK5dtD8vD7cpJyG3iraFtkQM" 
     config.cdn_subdomain = true
-end
-  
+end 
